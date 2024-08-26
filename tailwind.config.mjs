38f250ab3d67e3fcebc0e0d36c3d296622ec7fb0
Luis Höfer <luis.hoefer@mirantus.com>
2024-08-26 23:52:37 +0200

    chore: add daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+import typography from "@tailwindcss/typography";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, daisyui],
+  daisyui: {
+    themes: ["fantasy", "dracula", "night"],
+  },
+  darkMode: ["class", '[data-theme="night"]'],
+};
