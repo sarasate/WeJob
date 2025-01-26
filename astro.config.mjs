3fa0c639611173724a03ec0a1eb149bb426b1dc6
Luis Höfer <luis.hoefer@mirantus.com>
2025-01-26 15:44:12 +0100

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://wejob.dev",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   // Generate sitemap
   sitemap: true,
 });
